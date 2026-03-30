afc5b74d527baf43f4405f940fd350b99a8b2197
smruti-shrabani <smrutishrabanimoharana@gmail.com>
2026-03-30 23:55:06 +0530

    checking with PostGreSQL

--- server.js
+++ server.js
@@ -1,11 +1,63 @@
 const express = require("express");
 var cors = require('cors')
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "movies",
+  password: "1805",
+  port: 5432,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json(),cors());
-const data = require("./Json-data/movie-data.json");
+// app.use(express.json(),cors());
+// const data = require("./Json-data/movie-data.json");
+
+// app.get("/",(req,res)=>
+// {
+//   res.send({msg:"/movies for all movies list and /movies/ID for valid id input"})
+// })
+
+// app.get("/movies", (req, res) => {
+//   res.send(data);
+// });
+
+// app.get("/movies/:id", (req, res) => {
+//   const movieId = req.params.id;
+//   const searchData = data.find((i) => i.id == movieId);
+//   if (searchData) {
+//     res.send(searchData);
+//   } else {
+//     //impelement 404 not found with a json
+//     res.send({msg:"No data with this ID"});
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+
+db.connect();
+
+let movies = [];
+db.query("SELECT * FROM movies_data", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    movies = res.rows;
+  }
+  db.end();
+});
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 app.get("/",(req,res)=>
 {
@@ -13,7 +65,7 @@ app.get("/",(req,res)=>
 })
 
 app.get("/movies", (req, res) => {
-  res.send(data);
+  res.send(movies);
 });
 
 app.get("/movies/:id", (req, res) => {
@@ -27,8 +79,5 @@ app.get("/movies/:id", (req, res) => {
   }
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running at http://localhost:${PORT}`);
-// });
 
 module.exports = app;
