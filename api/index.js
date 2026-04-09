7c36e5c73874a2fbb5676412f495313ca6a30a60
smruti-shrabani <smrutishrabanimoharana@gmail.com>
2026-04-09 07:16:18 +0530

    checking with PostGreSQL error fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,55 @@
-// api/index.js
-const app = require('../server.js');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("../db"); // adjust path
+
+const app = express();
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "movies",
+  password: "1805",
+  port: 5432,
+});
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send({
+    msg: "/movies for all movies list and /movies/ID for valid id input",
+  });
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM movies_data");
+    res.send(result.rows);
+  } catch (err) {
+    res.status(500).send({ msg: "Database error" });
+  }
+});
+
+app.get("/movies/:id", async (req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT * FROM movies_data WHERE id = $1",
+      [req.params.id]
+    );
+
+    if (result.rows.length > 0) {
+      res.send(result.rows[0]);
+    } else {
+      res.status(404).send({ msg: "No data with this ID" });
+    }
+  } catch (err) {
+    res.status(500).send({ msg: "Database error" });
+  }
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,21 +1,64 @@
-const express = require("express");
-const cors = require('cors')
-const bodyParser = require("body-parser");
-const pg = require("pg");
+// const express = require("express");
+// const cors = require('cors')
+// const bodyParser = require("body-parser");
+// const pg = require("pg");
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "movies",
-  password: "1805",
-  port: 5432,
-});
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "movies",
+//   password: "1805",
+//   port: 5432,
+// });
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-// app.use(express.json(),cors());
-// const data = require("./Json-data/movie-data.json");
+// // app.use(express.json(),cors());
+// // const data = require("./Json-data/movie-data.json");
+
+// // app.get("/",(req,res)=>
+// // {
+// //   res.send({msg:"/movies for all movies list and /movies/ID for valid id input"})
+// // })
+
+// // app.get("/movies", (req, res) => {
+// //   res.send(data);
+// // });
+
+// // app.get("/movies/:id", (req, res) => {
+// //   const movieId = req.params.id;
+// //   const searchData = data.find((i) => i.id == movieId);
+// //   if (searchData) {
+// //     res.send(searchData);
+// //   } else {
+// //     //impelement 404 not found with a json
+// //     res.send({msg:"No data with this ID"});
+// //   }
+// // });
+
+// // app.listen(PORT, () => {
+// //   console.log(`Server is running at http://localhost:${PORT}`);
+// // });
+
+// db.connect();
+
+// let movies = [];
+// db.query("SELECT * FROM movies_data", (err, res) => {
+//   if (err) {
+//     console.error("Error executing query", err.stack);
+//   } else {
+//     movies = res.rows;
+//   }
+//   db.end();
+// });
+
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+
 
 // app.get("/",(req,res)=>
 // {
@@ -23,12 +66,16 @@ const PORT = process.env.PORT || 3000;
 // })
 
 // app.get("/movies", (req, res) => {
-//   res.send(data);
+//   if (movies.length === 0) {
+//     res.status(500).send({ msg: "Failed to fetch movies from the database." });
+//   } else {
+//     res.send(movies);
+//   }
 // });
 
 // app.get("/movies/:id", (req, res) => {
 //   const movieId = req.params.id;
-//   const searchData = data.find((i) => i.id == movieId);
+//   const searchData = movies.find((i) => i.id == movieId);
 //   if (searchData) {
 //     res.send(searchData);
 //   } else {
@@ -37,51 +84,4 @@ const PORT = process.env.PORT || 3000;
 //   }
 // });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running at http://localhost:${PORT}`);
-// });
-
-db.connect();
-
-let movies = [];
-db.query("SELECT * FROM movies_data", (err, res) => {
-  if (err) {
-    console.error("Error executing query", err.stack);
-  } else {
-    movies = res.rows;
-  }
-  db.end();
-});
-
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-
-app.get("/",(req,res)=>
-{
-  res.send({msg:"/movies for all movies list and /movies/ID for valid id input"})
-})
-
-app.get("/movies", (req, res) => {
-  if (movies.length === 0) {
-    res.status(500).send({ msg: "Failed to fetch movies from the database." });
-  } else {
-    res.send(movies);
-  }
-});
-
-app.get("/movies/:id", (req, res) => {
-  const movieId = req.params.id;
-  const searchData = movies.find((i) => i.id == movieId);
-  if (searchData) {
-    res.send(searchData);
-  } else {
-    //impelement 404 not found with a json
-    res.send({msg:"No data with this ID"});
-  }
-});
-
-module.exports = app;
+// module.exports = app;
